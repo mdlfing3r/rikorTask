b98cf55c3d3d60cc6bee9b01d78c6cd30b3305fc
Alexandr <mdlfing3r@gmail.com>
2024-02-03 16:40:18 +0300

    Правки

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
-#include <list>
 #include <stdlib.h>
+#include <algorithm>
 
 #define ROW_AMOUNT 8192
 #define COLUMN_AMOUNT 8192
@@ -16,7 +16,6 @@ typedef struct Coordinate {
     uint16_t Xpos;
     uint16_t Ypos;
 } Coordinate;
-
 
 
 uint64_t numResolver(uint64_t FirstNum, uint64_t numPos) {
@@ -35,15 +34,10 @@ uint64_t numResolver(uint64_t FirstNum, uint64_t numPos) {
 
 
 
-void Generator()
+std::vector<uint64_t> Generator()
 {
-
     std::vector<uint64_t> Arr;
-
-
-    //for (auto it = 0; it < ROW_AMOUNT; it++) LogicArr.push_back(0);
-    //for (auto it = 0; it < COLUMN_AMOUNT-1; it++) arr.push_back(LogicArr);
-
+    std::vector<std::vector<uint64_t>> ArrList;
 
     uint64_t beginNumber,
              tempval = 0;
@@ -56,25 +50,48 @@ void Generator()
 
         for (auto column = 0; column < ROW_AMOUNT; column++) { //столбец
 
-            if (tempval > maxVALUE) beginNumber = row;
-
+            if (tempval > maxVALUE) beginNumber = row; //если следовать ТЗ п. 4) то начальное значение вообще не будет меняться   
             *&tempval = numResolver(beginNumber, column);
 
             Arr.push_back(*&tempval);
 
-        }
-       
+        }      
     }
+    return Arr;
 }
 
-bool Finder(uint64_t F1, uint64_t F2, uint64_t F3, Coordinate Position) {
-    
+
+bool Finder(std::vector<uint64_t> array, uint64_t F1, uint64_t F2, uint64_t F3, Coordinate &Position) {
+
+    std::vector<uint64_t>::iterator iter = array.begin();
+
+    while ((iter = std::find(iter, array.end(), F2)) != array.end())
+    {
+        if (iter > array.begin() && iter < array.end()) {
+
+            if (*(iter + 8192) == F1) {
+                if (*(iter + (8192 * 2)) == F3) {
+                    Position.Ypos = (iter - array.begin()) / 8192;
+                    Position.Xpos = (iter - array.begin()) % 8192;
+                }
+            }
+        }
+
+        iter++;
+    }
+
+    return 1;
 }
 
 
 int main() {
-    Generator();
-    Finder()
+    Coordinate coord;
 
+    if (Finder(Generator(), 128, 64, 256, coord))
+        std::cout << "X = " << coord.Xpos << " Y = " << coord.Ypos;
+    else
+        std::cout << "can`t resolve coordinate";
+
+    
     return 0;
 }
